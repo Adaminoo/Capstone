3acbd4b94d7017aa225c6190d1763af51a9f6608
Tyler VanSickle <tylervansickle@Tylers-MacBook-Pro.local>
2025-04-07 12:16:45 -0600

    Redid a bunch of code to make it work with the GET /api/profile request

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -24,33 +24,25 @@ exports.login = async (req, res) => {
       return res.status(401).json({ message: "Invalid username or password" });
     }
 
-    const token = generateToken(user.id);
+    const token = generateToken(user.user_id); 
+    console.log("Generated Token:", token);  // Debugging line
     res.json({ message: "Login successful", token });
   } catch (err) {
     console.error("Error logging in:", err);
     res.status(500).json({ message: "Failed to log in" });
   }
 };
-
 // REGISTER ROUTE (POST)
 // This is for basic signup, I will make another one for Admins signing up other Admins.
 exports.signup = async (req, res) => {
-  const { username, firstName, lastName, email, birthday, password, isAdmin } =
-    req.body;
+  const { username, firstName, lastName, email, birthday, password, isAdmin } = req.body;
 
   // Validate fields
-  if (
-    !username ||
-    !firstName ||
-    !lastName ||
-    !email ||
-    !birthday ||
-    !password
-  ) {
+  if (!username || !firstName || !lastName || !email || !birthday || !password) {
     return res.status(400).json({ message: "All fields are required" });
   }
 
-  // Check if username already exists
+  // Check if username and email already exist
   const checkUsername = "SELECT * FROM users WHERE username = $1";
   const checkEmail = "SELECT * FROM users WHERE email = $1";
 
@@ -84,9 +76,10 @@ exports.signup = async (req, res) => {
       isAdmin,
     ]);
 
+    // Return the user_id (not just id)
     res.status(201).json({
       message: "User created successfully",
-      user_id: result.rows[0].id,
+      user_id: result.rows[0].user_id, 
     });
   } catch (err) {
     console.error("Error creating user:", err);
--- server/src/controllers/courseController.js
+++ server/src/controllers/courseController.js
@@ -1,5 +1,6 @@
 const db = require("../config/db");
 
+// Helper functions to check if a user or course exists, and if the user is already registered
 const checkUserExists = async (user_id) => {
   const userQuery = "SELECT * FROM users WHERE id = $1";
   const result = await db.query(userQuery, [user_id]);
@@ -18,14 +19,16 @@ const checkUserRegistration = async (user_id, course_id) => {
   const result = await db.query(checkQuery, [user_id, course_id]);
   return result.rows[0].count > 0;
 };
+
+// Get courses for the authenticated user
 const getCoursesForUser = async (req, res) => {
   const user_id = req.user_id;
   const query = `
-        SELECT c.course_id, c.name, c.description
-        FROM courses c
-        JOIN user_courses uc ON c.course_id = uc.course_id
-        WHERE uc.user_id = $1
-    `;
+    SELECT c.course_id, c.name, c.description
+    FROM courses c
+    JOIN user_courses uc ON c.course_id = uc.course_id
+    WHERE uc.user_id = $1
+  `;
   try {
     const results = await db.query(query, [user_id]);
     res.json(results.rows);
@@ -41,23 +44,19 @@ const registerForCourse = async (req, res) => {
   const { course_id } = req.params;
 
   try {
-    console.log(
-      "Attempting to register user:",
-      user_id,
-      "for course:",
-      course_id
-    );
+    console.log("Attempting to register user:", user_id, "for course:", course_id);
 
+    // Check if the user is already registered for the course
     if (await checkUserRegistration(user_id, course_id)) {
-      return res
-        .status(400)
-        .json({ message: "You are already registered for this course" });
+      return res.status(400).json({ message: "You are already registered for this course" });
     }
 
+    // Check if the course exists
     if (!(await checkCourseExists(course_id))) {
       return res.status(404).json({ message: "Course not found" });
     }
 
+    // Check course capacity
     const capacityQuery =
       "SELECT maximum_capacity, COUNT(*) AS current_registration FROM user_courses WHERE course_id = $1 GROUP BY course_id";
     const capacityResult = await db.query(capacityQuery, [course_id]);
@@ -69,19 +68,17 @@ const registerForCourse = async (req, res) => {
       courseCapacity = capacityResult.rows[0].maximum_capacity;
       currentRegistrationCount = capacityResult.rows[0].current_registration;
     }
+
     if (currentRegistrationCount >= courseCapacity) {
-      return res
-        .status(400)
-        .json({ message: "Course is full, cannot register" });
+      return res.status(400).json({ message: "Course is full, cannot register" });
     }
 
+    // Register the user for the course
     const insertQuery =
       "INSERT INTO user_courses (user_id, course_id) VALUES ($1, $2)";
     await db.query(insertQuery, [user_id, course_id]);
 
-    console.log(
-      `User ${user_id} successfully registered for course ${course_id}`
-    );
+    console.log(`User ${user_id} successfully registered for course ${course_id}`);
     res.status(201).json({ message: "Successfully registered for the course" });
   } catch (err) {
     console.error("Error in registerForCourse:", err);
@@ -89,20 +86,22 @@ const registerForCourse = async (req, res) => {
   }
 };
 
+// Unregister a user from a course
 const unregisterFromCourse = async (req, res) => {
   const user_id = req.user_id;
   const { course_id } = req.params;
 
   try {
+    // Delete the registration entry for the user and course
     const deleteQuery =
       "DELETE FROM user_courses WHERE user_id = $1 AND course_id = $2";
     const result = await db.query(deleteQuery, [user_id, course_id]);
 
+    // Check if the deletion was successful
     if (result.rowCount === 0) {
-      return res
-        .status(404)
-        .json({ message: "You are not registered for this course" });
+      return res.status(404).json({ message: "You are not registered for this course" });
     }
+
     res.json({ message: "Successfully unregistered from the course" });
   } catch (err) {
     console.error("Error in unregisterFromCourse:", err);
@@ -110,57 +109,61 @@ const unregisterFromCourse = async (req, res) => {
   }
 };
 
+// Admin register a user for a course
 const adminRegisterUserForCourse = async (req, res) => {
   const { user_id, course_id } = req.params;
 
   try {
+    // Check if the user exists
     if (!(await checkUserExists(user_id))) {
       return res.status(404).json({ message: "User not found" });
     }
 
+    // Check if the course exists
     if (!(await checkCourseExists(course_id))) {
       return res.status(404).json({ message: "Course not found" });
     }
 
+    // Check if the user is already registered for the course
     if (await checkUserRegistration(user_id, course_id)) {
-      return res
-        .status(400)
-        .json({ message: "User is already registered for this course" });
+      return res.status(400).json({ message: "User is already registered for this course" });
     }
 
+    // Register the user for the course
     const insertQuery =
       "INSERT INTO user_courses (user_id, course_id) VALUES ($1, $2)";
     await db.query(insertQuery, [user_id, course_id]);
 
-    res
-      .status(201)
-      .json({ message: "User successfully registered for the course" });
+    res.status(201).json({ message: "User successfully registered for the course" });
   } catch (err) {
     console.error("Error in adminRegisterUserForCourse:", err);
     return res.status(500).json({ message: "Database error" });
   }
 };
 
+// Admin unregister a user from a course
 const adminUnregisterUserFromCourse = async (req, res) => {
   const { user_id, course_id } = req.params;
 
   try {
+    // Check if the user exists
     if (!(await checkUserExists(user_id))) {
       return res.status(404).json({ message: "User not found" });
     }
 
+    // Check if the course exists
     if (!(await checkCourseExists(course_id))) {
       return res.status(404).json({ message: "Course not found" });
     }
 
+    // Delete the registration entry for the user and course
     const deleteQuery =
       "DELETE FROM user_courses WHERE user_id = $1 AND course_id = $2";
     const result = await db.query(deleteQuery, [user_id, course_id]);
 
+    // Check if the deletion was successful
     if (result.rowCount === 0) {
-      return res
-        .status(404)
-        .json({ message: "User is not registered for this course" });
+      return res.status(404).json({ message: "User is not registered for this course" });
     }
 
     res.json({ message: "User successfully unregistered from the course" });
