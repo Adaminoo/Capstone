373135477da06f91f5fcd25e820727ada22fea80
Tyler VanSickle <tylervansickle@Tylers-MacBook-Pro.local>
2025-04-30 18:00:43 -0600

    Changed DB stuff

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -4,7 +4,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
+  connectionString: `postgresql://admin:O2avpXMWGQUnKawlzyA1OKTJqSqtDX9f@dpg-d09bdp6uk2gs73d3a6j0-a:5432/mtech_pwid`,
+  // postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}
   // host: process.env.DB_HOST, //dpg-cuvk7qt2ng1s738pu1cg-a
   // user: process.env.DB_USER, //appuser
   // password: process.env.DB_PASSWORD, //BnM7zZKnoGqeMjPg1QP0TlKKdTRdFATC
